7918837858b73b7fb003ad32cb64615fff48b473
Team 2 <laptrinhdd2@gmail.com>
2019-03-13 10:12:10 +0700

    Project Android 2 Master
    Create project

---
+++ MediaMusicApp/app/src/main/java/android2/team2/mediamusicapp/MainActivity.java
@@ -0,0 +1,13 @@
+package android2.team2.mediamusicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
